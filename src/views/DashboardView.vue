b9858072273f4340a52b8217a9b5bee4c56e8b31
Johnata Matheus <johnata@paytour.com.br>
2025-03-02 22:32:41 -0300

    feat: create dashbord view

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,202 @@
+<script setup lang="ts">
+import { computed, h, ref, watch } from 'vue'
+import DataTable from '@/components/DataTable.vue'
+import TitleBar from '@/components/TitleBar.vue'
+import Input from '@/components/ui/input/Input.vue'
+import Label from '@/components/ui/label/Label.vue'
+import Button from '@/components/ui/button/Button.vue'
+import Checkbox from '@/components/ui/checkbox/Checkbox.vue'
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+} from '@/components/ui/select'
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
+} from '@/components/ui/dialog'
+import { LoaderIcon, CaseSensitiveIcon, FlagIcon, CheckIcon, XOctagonIcon } from 'lucide-vue-next'
+import type { ColumnDef } from '@tanstack/vue-table'
+import type { ITask } from '@/interfaces/Itask'
+import { useCreateTask, useTasks } from '@/composables/tasks'
+import { useToast } from '@/components/ui/toast'
+import StatusCard from '@/components/StatusCard.vue'
+
+const { data: tasks, isLoading } = useTasks()
+const { mutate: createTask, isPending } = useCreateTask()
+const { toast } = useToast()
+
+const safeTasks = computed(() => tasks.value ?? [])
+
+const totalTasks = computed(() => tasks.value?.length ?? 0)
+
+const isDialogOpen = ref(false)
+
+const newTask = ref<Partial<ITask>>({
+  title: '',
+  description: '',
+  status: 'pending',
+})
+
+const handleCreateTask = () => {
+  createTask(newTask.value as ITask, {
+    onSuccess: () => {
+      closeDialog()
+      toast({
+        title: 'Tarefa criada com sucesso!',
+        description: h('div', { class: 'flex items-center gap-2' }, [
+          h(CheckIcon, { class: 'text-green-500 w-5 h-5' }),
+          h('span', 'A nova tarefa foi adicionada.'),
+        ]),
+        variant: 'default',
+        duration: 4000,
+      })
+    },
+    onError: (_) => {
+      toast({
+        title: 'Erro ao criar tarefa',
+        description: h('div', { class: 'flex items-center gap-2' }, [
+          h(XOctagonIcon, { class: 'w-5 h-5 text-white' }),
+          h('span', 'Ocorreu um erro inesperado.'),
+        ]),
+        variant: 'destructive',
+        duration: 4000,
+      })
+    },
+  })
+}
+
+const defaultTask: Partial<ITask> = {
+  title: '',
+  description: '',
+  status: 'pending',
+}
+
+const openDialog = () => {
+  isDialogOpen.value = true
+}
+const closeDialog = () => {
+  isDialogOpen.value = false
+}
+
+const columns: ColumnDef<ITask>[] = [
+  {
+    accessorKey: 'checkbox',
+    header: () =>
+      h('div', { class: 'flex items-center gap-2' }, [
+        h(Checkbox, {
+          checked: true,
+          disabled: true,
+        }),
+      ]),
+    cell: () => h('div', h(Checkbox)),
+  },
+  {
+    accessorKey: 'title',
+    header: () =>
+      h('div', { class: 'flex items-center gap-2' }, [h(CaseSensitiveIcon), h('div', 'Titulo')]),
+    cell: ({ row }) => h('div', row.getValue('title')),
+  },
+  {
+    accessorKey: 'description',
+    header: () =>
+      h('div', { class: 'flex items-center gap-2' }, [h(CaseSensitiveIcon), h('div', 'Descrição')]),
+    cell: ({ row }) => h('div', row.getValue('description')),
+  },
+  {
+    accessorKey: 'status',
+    header: () =>
+      h('div', { class: 'flex items-center gap-2' }, [
+        h(FlagIcon, { class: 'scale-90' }),
+        h('div', h('strong', 'Status')),
+      ]),
+    cell: ({ row }) => {
+      const status = row.getValue('status') as ITask['status']
+      const statusColor =
+        status === 'completed'
+          ? 'text-green-500'
+          : status === 'pending'
+            ? 'text-yellow-500'
+            : 'text-blue-500'
+      return h('div', { class: `font-semibold ${statusColor}` }, status)
+    },
+  },
+]
+
+watch(isDialogOpen, (isOpen) => {
+  if (!isOpen) {
+    newTask.value = { ...defaultTask }
+  }
+})
+</script>
+
+<template>
+  <div
+    class="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10"
+  >
+    <div class="mx-auto grid w-full max-w-6xl gap-2">
+      <TitleBar
+        title="Tasks"
+        subtitle="Testando o subtitulo se está funcionando"
+        text-button="Criar Tarefa"
+        @button-click="openDialog"
+      />
+
+      <div class="mx-auto grid w-full max-w-6xl">
+        <StatusCard
+          status="Em progresso"
+          :icon="LoaderIcon"
+          :status-count="totalTasks"
+          background-color="bg-yellow-100"
+          text-color="text-yellow-600"
+          border-color="border-yellow-500"
+        />
+        <DataTable :columns="columns" :data="safeTasks" />
+      </div>
+    </div>
+
+    <Dialog v-model:open="isDialogOpen">
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Criar Nova Tarefa</DialogTitle>
+          <DialogDescription>Preencha os campos abaixo com os detalhes da tarefa</DialogDescription>
+        </DialogHeader>
+        <div class="grid gap-4">
+          <div>
+            <Label for="title"> Título </Label>
+            <Input id="title" v-model="newTask.title" />
+          </div>
+          <div>
+            <Label for="description"> Descrição </Label>
+            <Input id="description" v-model="newTask.description" />
+          </div>
+          <div>
+            <Label for="status"> Status </Label>
+            <Select v-model="newTask.status">
+              <SelectTrigger>
+                <SelectValue placeholder="Selecione o status" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="pending"> Pendente </SelectItem>
+                  <SelectItem value="in_progress"> Em progresso </SelectItem>
+                  <SelectItem value="completed"> Concluído </SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+        <DialogFooter>
+          <Button @click="handleCreateTask()">Salvar</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  </div>
+</template>
