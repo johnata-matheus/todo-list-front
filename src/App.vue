cc4a0949080690e815dd29bb3b8c07ffb78bb107
Johnata Matheus <johnata@paytour.com.br>
2025-03-02 22:31:30 -0300

    feat: add routerView and toaster in app

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import { VueQueryDevtools } from '@tanstack/vue-query-devtools'
+import Header from '@/components/layout/Header.vue'
+import { Toaster } from '@/components/ui/toast';
 </script>
 
 <template>
   <div>
+    <Header />
     <main>
-      <h1>teste</h1>
+      <RouterView />
     </main>
+    <Toaster />
   </div>
   <VueQueryDevtools />
 </template>
+
